a3f3babf808238eb39a9fa5ffcc5f610b97dc604
Cathy Ferrell <cferrell@rogers.com>
2017-08-20 19:13:05 -0400

    herewego

---
+++ wp-content/themes/twentyseventeen-child/index.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+ ?><!DOCTYPE html>
+<html  class="no-js no-svg" id="top">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Judson:400,400i,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Quando" rel="stylesheet">
+
+<?php wp_head(); ?>
+</head>
+
+<body>
+      <div id="picture">
+        <h1>COMING SOON!</h1>
+        <div id="itinerary">
+        <div id="month">
+          <h2>OCTOBER</h2>
+          <h3>Split, Croatia</h3>
+        </div>
+        <div id="month">
+          <h2>NOVEMBER</h2>
+          <h3>Prague, Czech Republic</h3>
+        </div>
+        <div id="month">
+          <h2>DECEMBER</h2>
+          <h3>Lisbon, Portugal</h3>
+        </div>
+        <div id="month">
+          <h2>JANUARY</h2>
+          <h3>Kyoto, Japan</h3>
+        </div>
+        <div id="month">
+          <h2>FEBRUARY</h2>
+          <h3>Chiang Mai, Thailand</h3>
+        </div>
+        <div id="month">
+          <h2>MARCH</h2>
+          <h3>Kuala Lumpur, Malaysia</h3>
+        </div>
+        <div id="month">
+          <h2>APRIL</h2>
+          <h3>Buenos Aires, Argentina</h3>
+        </div>
+        <div id="month">
+          <h2>MAY</h2>
+          <h3>Cordoba, Argentina</h3>
+        </div>
+        <div id="month">
+          <h2>JUNE</h2>
+          <h3>Lima, Peru</h3>
+        </div>
+        <div id="month">
+          <h2>JULY</h2>
+          <h3>Medellin, Columbia</h3>
+        </div>
+        <div id="month">
+          <h2>AUGUST</h2>
+          <h3>Bogota, Columbia</h3>
+        </div>
+        <div id="month">
+          <h2>SEPTEMBER</h2>
+          <h3>Mexico City, Mexico</h3>
+        </div>
+      </div>
+      </div>
+
+    </body>
+</html>
